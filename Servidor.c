b1db810e53cd6c153ff60d456c4e9d065c6506a9
Pedrorodriguez016 <146534432+Pedrorodriguez016@users.noreply.github.com>
2023-11-07 18:17:43 +0100

    cambio en el puerto

--- Servidor.c
+++ Servidor.c
@@ -638,7 +638,7 @@ int main(int argc, char *argv[])
 	//htonl formatea el numero que recibe al formato necesario
 	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
 	// establecemos el puerto de escucha
-	serv_adr.sin_port = htons(50008);
+	serv_adr.sin_port = htons(50009);
 	if (bind(sock_listen, (struct sockaddr *) &serv_adr, sizeof(serv_adr)) < 0)
 		printf ("Error al bind\n");
 	
