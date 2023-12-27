8cc316a7d2e7cdaf2f1f5def598b3550988f78ae
Danielonn <daniel.gomez.f@estudiantat.upc.edu>
2023-12-27 19:11:30 +0100

    movimiento pelota
    primer retoque del movimiento de la pelota

--- Cliente/Juego.Designer.cs
+++ Cliente/Juego.Designer.cs
@@ -35,6 +35,7 @@ namespace Cliente
             this.Jugador2 = new System.Windows.Forms.PictureBox();
             this.pelota = new System.Windows.Forms.PictureBox();
             this.TimerSalto = new System.Windows.Forms.Timer(this.components);
+            this.TimerPelota = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.Jugador1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Jugador2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pelota)).BeginInit();
@@ -44,9 +45,10 @@ namespace Cliente
             // 
             this.Jugador1.BackColor = System.Drawing.Color.Transparent;
             this.Jugador1.Image = global::Cliente.Properties.Resources.jugador_3;
-            this.Jugador1.Location = new System.Drawing.Point(587, 237);
+            this.Jugador1.Location = new System.Drawing.Point(629, 240);
+            this.Jugador1.Margin = new System.Windows.Forms.Padding(2);
             this.Jugador1.Name = "Jugador1";
-            this.Jugador1.Size = new System.Drawing.Size(76, 72);
+            this.Jugador1.Size = new System.Drawing.Size(51, 47);
             this.Jugador1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.Jugador1.TabIndex = 0;
             this.Jugador1.TabStop = false;
@@ -55,9 +57,10 @@ namespace Cliente
             // 
             this.Jugador2.BackColor = System.Drawing.Color.Transparent;
             this.Jugador2.Image = global::Cliente.Properties.Resources.jugador_2;
-            this.Jugador2.Location = new System.Drawing.Point(27, 220);
+            this.Jugador2.Location = new System.Drawing.Point(52, 229);
+            this.Jugador2.Margin = new System.Windows.Forms.Padding(2);
             this.Jugador2.Name = "Jugador2";
-            this.Jugador2.Size = new System.Drawing.Size(74, 89);
+            this.Jugador2.Size = new System.Drawing.Size(49, 58);
             this.Jugador2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.Jugador2.TabIndex = 1;
             this.Jugador2.TabStop = false;
@@ -66,10 +69,9 @@ namespace Cliente
             // 
             this.pelota.BackColor = System.Drawing.Color.Transparent;
             this.pelota.Image = global::Cliente.Properties.Resources.pelota;
-            this.pelota.Location = new System.Drawing.Point(349, 247);
-            this.pelota.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pelota.Location = new System.Drawing.Point(347, 240);
             this.pelota.Name = "pelota";
-            this.pelota.Size = new System.Drawing.Size(60, 62);
+            this.pelota.Size = new System.Drawing.Size(40, 40);
             this.pelota.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pelota.TabIndex = 2;
             this.pelota.TabStop = false;
@@ -78,17 +80,22 @@ namespace Cliente
             // 
             this.TimerSalto.Tick += new System.EventHandler(this.Temporizador_Tick);
             // 
+            // TimerPelota
+            // 
+            this.TimerPelota.Tick += new System.EventHandler(this.TimerPelota_Tick);
+            // 
             // Juego
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(738, 414);
+            this.ClientSize = new System.Drawing.Size(736, 334);
             this.Controls.Add(this.pelota);
             this.Controls.Add(this.Jugador2);
             this.Controls.Add(this.Jugador1);
             this.ForeColor = System.Drawing.SystemColors.ControlLight;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Juego";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.Text = "Juego";
@@ -108,5 +115,6 @@ namespace Cliente
         private System.Windows.Forms.PictureBox Jugador2;
         private System.Windows.Forms.PictureBox pelota;
         private System.Windows.Forms.Timer TimerSalto;
+        private System.Windows.Forms.Timer TimerPelota;
     }
 }
--- Cliente/Juego.cs
+++ Cliente/Juego.cs
@@ -14,10 +14,14 @@ namespace Cliente
     {
         public Juego()
         {
+            
             InitializeComponent();
             this.BackgroundImageLayout = ImageLayout.Stretch;
+            TimerPelota.Start();
         }
-        private int velocidadSalto = 10; 
+        
+        private int velocidadSalto = 10;
+        private int velocidadRebote = 10;
         private int alturaSalto = 100;    
         private int sueloY;               
         private bool enSalto = false;     // Variable para verificar si el personaje está en el aire
@@ -45,7 +49,6 @@ namespace Cliente
             }
             Point movimiento = new Point(x, y); // Creamos el nuevo punto a donde movimos el jugador
             Jugador1.Location = movimiento;
-         
 
         }
 
@@ -59,6 +62,7 @@ namespace Cliente
                 enSalto = true;
                 TimerSalto.Start();
             }
+            
 
         }
        
@@ -84,6 +88,70 @@ namespace Cliente
         private void Juego_Load(object sender, EventArgs e)
         {
             sueloY = 165;  // Posicion Y del juador - altura de la imagen del jugador: 237-72
+        }
+
+        private void TimerPelota_Tick(object sender, EventArgs e)
+        {
+            MoverPelota(); // Mueve la pelota
+            // Verifica colisiones y realiza el rebote si es necesario
+            VerificarColisiones();
+        }
+        private void MoverPelota()
+        {
+            int x = pelota.Location.X;
+            int y = pelota.Location.Y;
+
+            // Actualiza la posición de la pelota
+            pelota.Location = new Point(x - velocidadRebote, y + velocidadRebote);
+        }
+
+        private void VerificarColisiones()
+        {
+            int maxX = ClientSize.Width - pelota.Width;
+            int maxY = ClientSize.Height - pelota.Height;
+
+            // Verifica colisión con el suelo
+            if (pelota.Location.Y > maxY)
+            {
+                RealizarRebote(true, false); // Rebote en el eje Y
+                AjustarPosicionMaxY(); // Ajusta la posición de la pelota para evitar que pase el suelo
+            }
+
+            // Verifica colisión con el techo (opcional)
+            if (pelota.Location.Y < 0)
+            {
+                RealizarRebote(true, false); // Rebote en el eje Y
+                AjustarPosicionMinY(); // Ajusta la posición de la pelota para evitar que pase el techo
+            }
+
+            // Verifica colisión con los bordes laterales
+            if (pelota.Location.X < 0 || pelota.Location.X > maxX)
+            {
+                RealizarRebote(false, true); // Rebote en el eje X
+            }
+        }
+
+        private void RealizarRebote(bool enEjeY, bool enEjeX)
+        {
+            if (enEjeY)
+            {
+                velocidadRebote = -velocidadRebote; // Invierte la dirección del rebote en el eje Y
+            }
+
+            if (enEjeX)
+            {
+                velocidadRebote = +velocidadRebote; // Invierte la dirección del rebote en el eje X
+            }
+        }
+
+        private void AjustarPosicionMaxY()
+        {
+            pelota.Location = new Point(pelota.Location.X, ClientSize.Height - pelota.Height);
+        }
+
+        private void AjustarPosicionMinY()
+        {
+            pelota.Location = new Point(pelota.Location.X, 0);
         }
     }
 }
